b65077786646d28896eb214b2f60246faaea75bc
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-12-05 03:53:40 +0530

    feat: update RootLayout padding and redirect Home to dashboard

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
 	return (
-		<div className="min-h-svh w-full bg-white dark:bg-neutral-950 text-white flex items-center justify-center">
+		<div className="min-h-svh w-full bg-white dark:bg-neutral-950 text-white flex items-center justify-center px-5">
 			<Outlet />
 		</div>
 	);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { Navigate } from "react-router-dom";
+
 export default function Home() {
-	return <div>Home</div>;
+	return <Navigate to="/dashboard" replace />;
 }

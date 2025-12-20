8ebca45cbdf0b1d43908fcf3962d20dce61c0953
nikhilthakur8 <nikhilthakur8012004@gmail.com>
2025-12-21 02:15:22 +0530

    Fixed

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,20 +1,10 @@
 import AllowList from "@/components/AllowList";
 import DenyList from "@/components/DenyList";
 import { Button } from "@/components/ui/button";
-import { useUserContext } from "@/context/Context";
-import { LogOut, User } from "lucide-react";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
-	const { setUser } = useUserContext();
 	const [active, setActive] = useState<"denyList" | "allowList">("denyList");
-	const navigate = useNavigate();
-	function handleLogout() {
-		localStorage.removeItem("ADVOID_SESSION");
-		setUser(null);
-		navigate("/login");
-	}
 	return (
 		<div className="w-full my-4">
 			<div className="flex items-center max-w-3xl mx-auto justify-center flex-col gap-5 mt-10">
